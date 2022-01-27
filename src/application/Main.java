21fe80560b25320129ef7797c34fc14bf29187f7
MateusVieiraGDS <mateusvieirap010@gmail.com>
2022-01-27 01:16:08 -0300

    Main Construído, Melhoria em entities.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,77 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.OrderStatus;
+import entities.Product;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");		
+		
+		System.out.println("Enter cliente data:");
+				
+		Order order = new Order();
+		Client client = new Client();
+		
+		System.out.print("Name: ");
+		client.setName(sc.nextLine());
+		
+		System.out.print("Email: ");
+		client.setEmail(sc.nextLine());
+		
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		client.setBirthDate(sdf.parse(sc.nextLine()));
+		
+		order.setClient(client);
+		
+		System.out.println("Enter order data:");
+		
+		System.out.print("Status: ");
+		order.setStatus(OrderStatus.valueOf(sc.nextLine()));
+		
+		System.out.print("How many items to this order? ");
+		int n_items = sc.nextInt();
+		sc.nextLine();
+		
+		for(int i = 0; i < n_items; i++) {
+			System.out.printf("Enter #%d item data:%n", i + 1);
+			Product p = new Product();
+			
+			System.out.print("Product name: ");
+			p.setName(sc.nextLine());
+			
+			System.out.print("Product price: ");
+			double price = sc.nextDouble();
+			p.setPrice(price);
+			
+			OrderItem o_item = new OrderItem();
+			o_item.setProduct(p);
+			o_item.setPrice(price);
+			
+			System.out.print("Quantity: ");
+			o_item.setQuantity(sc.nextInt());
+			
+			sc.nextLine();
+			
+			order.addItem(o_item);
+		}
+		
+		order.setMoment(new Date());
+		
+		System.out.println(order);
+						
+		sc.close();
 	}
 
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -8,10 +9,12 @@ public class Order {
 	private Date moment;
 	private OrderStatus status;
 	private List<OrderItem> OrderItems = new ArrayList<>();
+	private Client client;
 	
 	public Order() {}
 
-	public Order(Date moment, OrderStatus status) {
+	public Order(Client client, Date moment, OrderStatus status) {
+		this.client = client;
 		this.moment = moment;
 		this.status = status;
 	}
@@ -31,6 +34,14 @@ public class Order {
 	public void setStatus(OrderStatus status) {
 		this.status = status;
 	}
+	
+	public Client getClient() {
+		return this.client;
+	}
+	
+	public void setClient(Client client) {
+		this.client = client;
+	}
 
 	public List<OrderItem> getOrderItems() {
 		return OrderItems;
@@ -49,4 +60,27 @@ public class Order {
 		}
 		return sum;
 	}
+	
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");	
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		StringBuilder result = new StringBuilder();
+		
+		result.append("\nORDER SUMMARY:");
+		
+		result.append("\nOrder moment: " + sdf2.format(getMoment()));
+		result.append("\nOrder status: " + getStatus());
+		result.append(String.format("%nClient: %s (%s) - %s", getClient().getName(), sdf.format(getClient().getBirthDate()), getClient().getEmail()));
+		result.append("\nOrder items:");
+		
+		for (OrderItem o_item : getOrderItems()) {
+			result.append( String.format("%n%s, $%.2f, Quantity: %d, Subtotal: $%.2f", o_item.getProduct().getName(), o_item.getPrice(), o_item.getQuantity(), o_item.subTotal()) );
+		}
+		
+		result.append(String.format("%nTotal price: $%.2f%n", total()));
+		
+		return result.toString();
+	} 
 }
